02c78220ef16cb177b571278f4e0754947afd9a2
dcmallett <dcmallett28@gmail.com>
2021-11-07 22:03:44 +0000

    Added the signup component and installed firebase

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,13 +1,48 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import styles from './Signup.module.css';
 
 
 const Signup = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayName, setDisplayName] = useState('');
+
+
+    const submitFormHandler = (e) => {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
     return (
         <Fragment>
-            <div>
-                <h2>Signup</h2>
-            </div>
+            <form onSubmit={submitFormHandler} className={styles['signup-form']}>
+                <label>
+                    <span>Email:</span>
+                    <input 
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email} 
+                    />
+                </label>
+                <label>
+                    <span>Password</span>
+                    <input 
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </label>
+                <label>
+                    <span>Display Name:</span>
+                    <input 
+                    type="text" 
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                    />
+                </label>
+                <button className='btn'>Signup</button>
+            </form>
         </Fragment>
     )
 }
